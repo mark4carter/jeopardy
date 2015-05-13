eeaed2cea485c167a01aa4898461ee03a0e90eb3
Mark Carter <mark4carter@users.noreply.github.com>
2015-05-13 12:31:09 -0400

    added question bank and box

---
+++ questBank.js
@@ -0,0 +1,56 @@
+bnk0a01 = ["Which one of these is an animal that starts with the letter 'a'?", "apple", "bananas", "cheese", "donkey", "1"];
+
+bnk0a02 = ["What is my favorite color?", "ice cream", "deadpool", "batman", "AMERICAA!", "1"];
+
+bnk0a03 = ["What is the capital of Washington DC", "New England", "New York", "That new new", "old face apple nose", "1"];
+
+bnk0a04 = ["The new watch from Apple is called", "S4", "S5", "S6", "S6 Edge", "1"];
+
+bnk0a05 = ["....you will never get this", "yes", "si", "no", "NEIN NEIN NEIN!!", "1"];
+
+
+bnk0a06 = ["Which of the following images should you use to deploy a portable workstation that has access to all applications on the intial start up, subsequent to the deployment", "A thick image", "A thin image", "A RIS image", "A disk miage", "1"];
+
+bnk0a07 = ["Prior to deployment, which option could you use to verify whether an image can be used", "Sysprep.exe", "Deploy and boot from VHD.", "System File Check (SFC.exe)", "Deployment Image Servicing and Management (DISM) tool", "2"];
+
+bnk0a08 = ["All workstations have Windows Vista.  Your have a current Windows 7 image, but are now instructed to change the current image to Windows 7 Enterprise.", "Use DISM to service the image online", "Use DISM to service the miage offline", "Use ImageX to service the image online", "Use ImageX to service the image online", "4"];
+
+bnk0a09 = ["You have created a Windows 7 image for deployment, However drives have been released since image was created.  How would you update the image with the new drivers?", "mount the image using ImageX, then use /Remove-Driver and /Add-driver", "mount using DISM, then use /Remove-Driver and /Add-Driver", "mount using DISM, then run the driver's exe file to install", "mount using ImageX, then run driver's exe file to install", "2"];
+
+bnk0a10 = ["You are instructed to deply a new accounting application that has to be installed in the context of the Local System Account.  Which action should you take?", "Deploy using a logon script", "Deploy using a logon script", "Configure GPO to assign application to the Sales users", "GPO to assign the application to the Sales computers, then reboot", "4"];
+
+//
+
+bnk0a11 = ["A software or hardware that checks information coming from the Internet and depending on the applied configuration settings either blocks it or allows it to pass through is called:", "Antivirus", "Firewall", "AntiSpyware", "Malware", "2"];
+
+bnk0a12 = ["A device designed to forward data packets between networks is called:", "Switch", "Hub", "Router", "MAC Filter", "3"];
+
+bnk0a13 = ["Which of the following answers refers to a dedicated device for managing secure connections established over an untrusted network, such as the Internet?", "Load balancer", "VPN concentrator", "SPAM filter", "Web server", "2"];
+
+bnk0a14 = ["Which of the following answers list the protocol or port number used by a spam filter? ", "HTTPS", "23", "443", "25", "4"];
+
+bnk0a15 = ["Which of the following network security solutions inspects network traffic in real-time and has the capability to stop the ongoing attack?", "NIPS", "HIDS", "HIPS", "NIST", "1"];
+
+//
+
+bnk0a16 = ["File Transfer Protocol (FTP) runs by default on port(s):", "20/21", "24/25", "80/443", "21", "1"];
+
+bnk0a17 = ["A technician uses an RDP client on their Windows OS in order to remotely troubleshoot a problem on another Windows machine. Which of the following ports needs to be opened for the built-in Windows RDP server to allow this type of network connection?", "1701", "139", "3389", "110", "3"];
+
+bnk0a18 = ["A network administrator has been asked to establish secure network link for exchanging files between two remote hosts. Which of the following ports should be in use to enable this type of network connection?", "23", "21", "20", "22", "4"];
+
+bnk0a19 = ["Which of the following answers refers to a port number for a lightweight protocol typically used for transferring boot files and configuration files between hosts in a secure Intranet environment?", "20", "69", "21", "22", "2"];
+
+bnk0a20 = ["A hacker has captured network traffic with cleartext commands sent from the client to the server console. Which of the following ports is being used by the network admin for the client-server communication?", "49", "23", "68", "22", "2"];
+
+//
+
+bnk0a21 = ["Which part of the AAA security architecture deals with the verification of the identity of a person or process?", "Authentication", "Authorization", "Accounting", "Content filtering", "1"];
+
+bnk0a22 = ["Which of the following answers refers to a data link layer (Layer 2) device designed to forward data packets between Local Area Network (LAN) segments?", "Hub", "Switch", "Router", "Firewall", "2"];
+
+bnk0a23 = ["A security solution implemented on an individual computer monitoring that specific system for malicious activities or policy violations is referred to as:", "Firewall", "HIDS", "MAC filter", "NIPS", "2"];
+
+bnk0a24 = ["Which of the devices listed below allows for establishing a communication channel over a PSTN?", "Default gateway", "ICS server", "Multilayer switch", "Analog modem", "4"];
+
+bnk0a04 = ["An IP PBX system is an example of:", "Unified voice services", "Backdoor access", "Reverse proxy", "Peer-to-Peer (P2P) architecture", "1"];
